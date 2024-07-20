59fea3eecacacb83c399e7b0c9bbdb1f35e45403
David <107300355+lordryns@users.noreply.github.com>
2024-07-20 15:52:23 +0100

    Add files via upload

---
+++ ambit.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+is_running = True
+
+
+def get_json_files() -> list['str']:
+    current = os.getcwd()
+    all_files = [f for f in os.listdir(current) if os.path.isfile(os.path.join(current, f))]
+    json_files = [j for j in all_files if j.split(".")[1] == "json"]
+
+    return json_files
+
+
+def get_json_script() -> tuple:
+    try:
+        script: str = [s for s in get_json_files() if s == "script.json"][0]
+        with open(script, 'r') as fp:
+            return json.load(fp), True
+    except IndexError as e:
+        return "Unable to find a script.json file in this directory.", False
+
+
+def check_for_changes():
+    response = get_json_script()
+    if response[1]:
+        json_code = response[0]
